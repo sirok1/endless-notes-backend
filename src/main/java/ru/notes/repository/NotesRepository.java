222c33dc2cb2f39a402997331971f3863fb2d83b
sirok1 <s1r0k.dev@gmail.com>
2024-03-04 22:29:58 +0300

    refactoring(dev): changed id types in Repositiry

--- src/main/java/ru/notes/repository/NotesRepository.java
+++ src/main/java/ru/notes/repository/NotesRepository.java
@@ -1,10 +1,6 @@
 package ru.notes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.notes.model.Note;
+import ru.notes.entity.Note;
 
-import java.util.List;
-
-public interface NotesRepository extends JpaRepository<Note, Integer> {
-
-}
+public interface NotesRepository extends JpaRepository<Note, String> {}
