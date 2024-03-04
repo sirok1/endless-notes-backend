f146ac308f09a1fec8c7eabd888490d99ac94ebd
sirok1 <s1r0k.dev@gmail.com>
2024-03-04 22:29:24 +0300

    refactoring(dev): clear unused imports

--- src/main/java/ru/notes/rest/NoteController.java
+++ src/main/java/ru/notes/rest/NoteController.java
@@ -4,8 +4,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.notes.model.Note;
+import ru.notes.entity.Note;
 import ru.notes.service.NoteService;
+import ru.notes.service.UserService;
 
 import java.util.List;
 
@@ -15,11 +16,10 @@ public class NoteController {
     private final NoteService noteService;
 
     public NoteController(NoteService noteService) {
-        this.noteService = noteService;
-    }
+        this.noteService = noteService;}
 
     @GetMapping(path = "/getAllNotes")
-    public ResponseEntity<List<Note>> getAllNotes () {
+    public ResponseEntity<List<Note>> getAllNotes() {
         return ResponseEntity.ok().body(noteService.getAllNotes());
     }
 }
