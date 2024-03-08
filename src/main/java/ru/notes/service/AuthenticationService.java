7483fd22257cc2473d534e40cb5c40ef5c06b892
sirok1 <s1r0k.dev@gmail.com>
2024-03-08 15:38:21 +0300

    feat(dev): jwt

---
+++ src/main/java/ru/notes/service/AuthenticationService.java
@@ -0,0 +1,15 @@
+package ru.notes.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+}
