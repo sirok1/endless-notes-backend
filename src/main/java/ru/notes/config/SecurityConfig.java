2ddb362640523bfde5aee5403e07db5ab282307b
sirok1 <s1r0k.dev@gmail.com>
2024-03-08 15:37:41 +0300

    feat(dev): SecurityConfig for spring

--- src/main/java/ru/notes/config/SecurityConfig.java
+++ src/main/java/ru/notes/config/SecurityConfig.java
@@ -3,14 +3,35 @@ package ru.notes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
-        security.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll());
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    return corsConfiguration;
+                }))
+                // настраиваются доступы для ролей
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .authenticationProvider()
         return security.build();
     }
+
+
 }
